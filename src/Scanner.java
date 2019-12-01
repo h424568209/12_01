3ca5b466851ce1bc2f89e14fb58f99c15a0d4bd5
h424568209 <53755235+h424568209@users.noreply.github.com>
2019-12-01 22:07:17 +0800

    Update Scanner.java

--- src/Scanner.java
+++ src/Scanner.java
@@ -2,21 +2,19 @@ import java.io.*;
 import java.util.Comparator;
 
 public class Scanner {
-    public Scanner(InputStream in) {
-    }
-
-    public static  void scannerDirectory(TreeNode node){
-        File[] file = node.file.listFiles();
-        if(file ==null){
+    public static void scannerDirectory(TreeNode node) {
+        File[] files = node.file.listFiles();
+        if (files == null) {
             return;
         }
-        for(File files:file){
+
+        for (File file : files) {
             TreeNode child = new TreeNode();
-            child.file = files;
-            if(files.isDirectory()){
+            child.file = file;
+            if (file.isDirectory()) {
                 scannerDirectory(child);
-            }else{
-                child.totalLength = file.length;
+            } else {
+                child.totalLength = file.length();
             }
             node.totalLength += child.totalLength;
             node.children.add(child);
@@ -27,13 +25,57 @@ public class Scanner {
         TreeNode root = new TreeNode();
         root.file = new File("C:\\");
         scannerDirectory(root);
-        PrintStream out  = new PrintStream(new FileOutputStream("C啊C.txt"),true,"UTF-8");
-//        Comparator<TreeNode > sotrByLengthDesc = new Comparator<TreeNode>() {
-//            @Override
-//            public int compare(TreeNode o1, TreeNode o2) {
-//                if(o1.)
-//            }
-//        }
+        PrintStream out = new PrintStream(
+                new FileOutputStream("扫描报告c.txt"),
+                true, "UTF-8");
+        Comparator<TreeNode> sortByLengthDesc = new Comparator<TreeNode>() {
+            @Override
+            public int compare(TreeNode o1, TreeNode o2) {
+                if (o1.totalLength > o2.totalLength) {
+                    return -1;
+                } else if (o1.totalLength == o2.totalLength) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+        };
 
+        report(root, 0, sortByLengthDesc, out);
+
+        out.close();
+    }
+
+    private static void report(TreeNode root, int level, Comparator<TreeNode> sortBy, PrintStream out) {
+        for (int i = 0; i < level * 4; i++) {
+            out.print(" ");
+        }
+        if (level == 0) {
+            out.print(root.file.getAbsolutePath());
+        } else {
+            out.print(root.file.getName());
+        }
+        if (root.file.isDirectory()) {
+            out.print("\\");
+        }
+
+        String unit = "字节";
+        double length = root.totalLength;
+        if (length > 1024 * 1024 * 1024) {
+            unit = "G字节";
+            length = length / 1024 / 1024 / 1024;
+        } else if (length > 1024 * 1024) {
+            unit = "M字节";
+            length = length / 1024 / 1024;
+        } else if (length > 1024) {
+            unit = "K字节";
+            length = length / 1024;
+        }
+        out.printf("    %.2f%s%n", length, unit);
+
+        root.children.sort(sortBy);
+        for (TreeNode node : root.children) {
+            report(node, level + 1, sortBy, out);
+        }
     }
 }
